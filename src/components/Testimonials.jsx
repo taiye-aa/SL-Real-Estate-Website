4ad185edf5e145b0831c15fbf9f9ad5598935ae0
Taiwo <taiwoaa04@gmail.com>
2025-10-31 00:19:41 +0100

    changes to mobile screen view

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -15,11 +15,12 @@ const Testimonials = () => {
         <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Customer <span className='underline underline-offset-4 under  decoration-1 font-light'>Testimonials</span></h1>
         <p  className='text-center text-gray-500 mb-12 mx-auto max-w-80'>Real Stories form those who found Home with Us</p>
         <div className='flex wrap justify-center gap-8'>
-            {testimonials.map((testimonial, id)=>{
+           <div className='flex flex-col gap-5 sm:flex-row'>
+                 {testimonials.map((testimonial, id)=>{
                 return(
-                    <div key={id} className='max-w-[340px] shadow-lg rounded px-8 py-12 text-center'>
+                    <div key={id} className=' max-w-[340px] shadow-xl bg-[#F5F5DC] rounded px-8 py-6 text-center'>
                         <img className='w-20 h-20 rounded-full mx-auto mb-4'src={testimonial.image} alt={testimonial.alt} />
-                        <h2 className='text-xl text-gray-700 font-medium'>{testimonial.name}</h2>
+                        <h2 className='text-lg sm:text-xl text-gray-700 font-medium'>{testimonial.name}</h2>
                         <p className='text-gray-500 mb-4 text-sm'>{testimonial.title}</p>
                         <div className='flex justify-center gap-1 text-red-500 mb-4'>
                             {Array.from({length: testimonial.rating}, (item,index)=>(
@@ -30,6 +31,7 @@ const Testimonials = () => {
                     </div>
                 )
             })}
+           </div>
         </div>
     </motion.div>
   )
